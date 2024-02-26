9286a06bb3e15e7c9df20e336a44c1b543057381
Salvador <sarevalotamayo@student.rtc.edu>
2024-02-25 17:34:21 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GA_Nested_If_Salvador_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
