6c98bcc78036a7d9c8628435609687a8aa45c3a5
Salvador <sarevalotamayo@student.rtc.edu>
2024-02-25 18:34:11 -0800

    update code

--- Program.cs
+++ Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+            if (age >= 18)
+            {
+                Console.Write("Are you a student (yes/no): ");
+                string studentStatus = Console.ReadLine();
+                if (studentStatus.ToLower() == "yes")
+                {
+                    Console.Write("Enter your GPA (0.0-4.0): ");
+                    double gpa = double.Parse(Console.ReadLine());
+
+                    if (gpa >= 3.0)
+                    {
+                        Console.WriteLine("Congratulations! You are an adult with a good GPA.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You are an adult student, but your GPA needs improvement.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("You are an adult, but not a student.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("You are not an adult.");
+            }
         }
     }
 }
